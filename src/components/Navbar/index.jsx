618814ab0477fdd8d1212e64312a4e311a06b08c
Oyedare <ooyedaretemiloluwa@gmail.com>
2022-11-16 02:33:24 +0100

    feat: add connect wallet modal

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyledNavbar } from './style'
 import Logo from '../../assets/logo.svg'
+import X from '../../assets/x'
+import Chevron from '../../assets/Chevron'
+import metaMask from '../../assets/metamask.svg'
+import wallet from '../../assets/wallet.svg'
+// import X from '../../assets/x.svg'
 const Navbar = () => {
+  const [isClicked,setIsClicked] = useState(false)
   return (
     <StyledNavbar>
         <div>
@@ -17,7 +23,42 @@ const Navbar = () => {
             <a href="/">Community</a>
         </div>
 
-        <div className="btn">Connect wallet</div>
+        <div className="btn" onClick={()=> setIsClicked(!isClicked)}>Connect wallet</div>
+        {isClicked && (
+          <div className="modal-bg">
+            <div className="modal">
+              <div className="modal-head">
+                <h4>Connect Wallet</h4>
+                <div className='X' onClick={()=>setIsClicked(!isClicked)}>
+                  <X />
+                </div>
+              </div>
+
+              <div className="modal-body">
+                <p>Choose your preferred wallet:</p>
+                <div className="meta-mask">
+                  <div className="text">
+                    <img src={metaMask} alt="meta mask" />
+                    <h4>Metamask</h4>
+                  </div>
+                  <div>
+                    <Chevron />
+                  </div>
+                </div>
+
+                <div className="wallet">
+                  <div className="text">
+                    <img src={wallet} alt="wallet connect" />
+                    <h4>WalletConnect</h4>
+                  </div>
+                  <div>
+                    <Chevron />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
     </StyledNavbar>
   )
 }
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -30,4 +30,86 @@ export const StyledNavbar = styled.nav`
         color: var(--white);
         cursor: pointer;
     }
+    .modal-bg{
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(64, 64, 64, 0.3);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .modal{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            background: #FFFFFF;
+            border-radius: 1rem;
+            width: 600px;
+            &-head{
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                align-items: center;
+                padding: 24px 32px;
+                width: 100%;
+                border-bottom: 1px solid #CFD8DC;
+                h4{
+                    font-weight: 700;
+                    font-size: 24px;
+                    line-height: 30px;
+                    color: #333333;
+                }
+                .X{
+                    cursor: pointer;
+                }
+            }
+            &-body{
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                padding: 32px;
+                gap: 16px;
+                width: 100%;
+                p{
+                    font-weight: 400;
+                    font-size: 16px;
+                    line-height: 20px;
+                    color: #333333;
+                }
+                .meta-mask,.wallet{
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                    align-items: center;
+                    padding: 12px 20px;
+                    gap: 135px;
+                    background: #F8F9FA;
+                    border: 1px solid #CFD8DC;
+                    border-radius: 12px;
+                    width: 100%;
+                    cursor: pointer;
+                    .text{
+                        display: flex;
+                        flex-direction: row;
+                        align-items: center;
+                        padding: 0px;
+                        gap: 16px;
+                        h4{
+                            font-weight: 600;
+                            font-size: 18px;
+                            line-height: 23px;
+                            color: #000000;
+                        }
+                    }
+                }
+                .wallet{
+                    background-color: var(--white);
+                }
+            }
+        }
+    }
 `
